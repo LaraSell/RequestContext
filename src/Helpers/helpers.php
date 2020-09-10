fe3a2768d937740b5078443932ffda6d055b45d9
Tehran <tehran_alishov@hotmail.com>
2020-09-11 00:23:53 +0400

    .

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -6,21 +6,21 @@ if (!function_exists('context')) {
         $args = func_get_args();
 
         if (count($args) === 0) {
-            return app(\LaraSell\RequestContext\Managers\ContexManager::class);
+            return app(\LaraSell\RequestContext\Managers\ContextManager::class);
         }
 
         if (count($args) === 1) {
             if (is_array($args[0])) {
                 foreach ($args[0] as $key => $value) {
-                    app(\LaraSell\RequestContext\Managers\ContexManager::class)->put($key, $value);
+                    app(\LaraSell\RequestContext\Managers\ContextManager::class)->put($key, $value);
                 }
 
                 return;
             }
 
-            return app(\LaraSell\RequestContext\Managers\ContexManager::class)->get($args[0]);
+            return app(\LaraSell\RequestContext\Managers\ContextManager::class)->get($args[0]);
         }
 
-        return app(\LaraSell\RequestContext\Managers\ContexManager::class)->put($args[0], $args[1]);
+        return app(\LaraSell\RequestContext\Managers\ContextManager::class)->put($args[0], $args[1]);
     }
 }
