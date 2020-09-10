51850b05410249c462b1df42bba5f8b01b53d191
Tehran <tehran_alishov@hotmail.com>
2020-09-10 22:08:08 +0400

    .

--- src/Helpers/helper.php
+++ src/Helpers/helper.php
@@ -1,1 +1,26 @@
 <?php
+
+if (!function_exists('context')) {
+    function context()
+    {
+        $args = func_get_args();
+
+        if (count($args) === 0) {
+            return app(\LaraSell\RequestContext\Managers\ContexManager::class);
+        }
+
+        if (count($args) === 1) {
+            if (is_array($args[0])) {
+                foreach ($args[0] as $key => $value) {
+                    app(\LaraSell\RequestContext\Managers\ContexManager::class)->put($key, $value);
+                }
+
+                return;
+            }
+
+            return app(\LaraSell\RequestContext\Managers\ContexManager::class)->get($args[0]);
+        }
+
+        return app(\LaraSell\RequestContext\Managers\ContexManager::class)->put($args[0], $args[1]);
+    }
+}
--- src/Managers/ContextManager.php
+++ src/Managers/ContextManager.php
@@ -2,6 +2,70 @@
 
 namespace LaraSell\RequestContext\Managers;
 
+use LaraSell\RequestContext\Exceptions\UnregisteredContextException;
+
 class ContextManager
 {
+    /**
+     * The collection of contexts
+     * 
+     * @var array
+     */
+    private $contexts = [];
+
+    /**
+     * Add a context to the collection
+     * 
+     * @param string|array $key
+     * @param mixed $value
+     * @return void
+     */
+    public function put($key, $value = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $arrayKey => $arrayValue) {
+                $this->bind((string) $arrayKey, $arrayValue);
+            }
+        } else {
+            $this->bind((string) $key, $value);
+        }
+    }
+
+    /**
+     * Get a value from the contexts
+     * 
+     * @param $key
+     * @throws UnregisteredContextException
+     * @return mixed
+     */
+    public function get($key)
+    {
+        if (isset($this->contexts[$key])) {
+            return $this->contexts[$key];
+        }
+
+        throw new UnregisteredContextException('No context registered for ' . $key);
+    }
+
+    /**
+     * Get all contexts
+     * 
+     * @return array
+     */
+    public function all()
+    {
+        return $this->contexts;
+    }
+
+    /**
+     * Store a key value pair into the collection
+     * 
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function bind(string $key, $value)
+    {
+        $this->contexts[$key] = $value;
+    }
 }
